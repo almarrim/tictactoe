0f1b6479423a447ab8ff521447d42737004c5acd
almarrim <mosalmarri@gmail.com>
2019-10-12 17:01:01 +0300

    1-organize  (moved/removed) called functions: (-)gamePlay from dumbComp, moved gameStatus response. It decreased the number called functions to about 50%. 2- move gameStatus announcement to a new function to create a delay to avoid announcing winner before showing the last move.3- add html game setting page(select opponent)+ css styles for the page.

--- tictactoe/js/app.js
+++ tictactoe/js/app.js
@@ -6,6 +6,9 @@ const greenClicked = [];
 const redClicked = [];
 let playerGreen = true;
 let playerRed = false;
+const players = ['Green', 'Red', 'Tie'];
+const x = 2;
+const playStyle = ['human', 'dumpComp']
 const turnGreen = function () {
     if (playerGreen)
         this.className += ' green';
@@ -19,6 +22,7 @@ const turnDefault = function () {
     this.className = 'items';
 }
 const clicked = function () {
+    console.log('enter clicked')
     this.removeEventListener('mouseenter', turnGreen);
     this.removeEventListener('mouseenter', turnRed);
     this.removeEventListener('mouseout', turnDefault);
@@ -32,6 +36,8 @@ const clicked = function () {
     this.classList.value += ' clicked';
     clickedItems.push(this);
     gamePlay();
+    console.log('leave clicked')
+
 }
 for (i of items) {
     i.addEventListener('mouseenter', turnGreen);
@@ -40,6 +46,8 @@ for (i of items) {
     i.addEventListener('click', clicked);
 }
 const restIt = function (x) {
+    console.log('enter resetIT')
+
 
     if (x == 'y') {
         for (i of items) {
@@ -63,18 +71,25 @@ const restIt = function (x) {
         playerGreen = true;
         playerRed = false;
         board.className = 'board';
+        winner = 2;
         return console.log('this is a new game');
     }
     else {
         board.className += ' zindex';
         return console.log('see ya later dogg');
     }
+    console.log('leave reset')
+
 }
 const gameStatus = function () {
+    console.log('enter gamestatus')
+
     if ((greenClicked.includes(items[0]) && greenClicked.includes(items[1]) && greenClicked.includes(items[2])) || (greenClicked.includes(items[3]) && greenClicked.includes(items[4]) && greenClicked.includes(items[5])) || (greenClicked.includes(items[6]) && greenClicked.includes(items[7]) && greenClicked.includes(items[8])) || (greenClicked.includes(items[0]) && greenClicked.includes(items[3]) && greenClicked.includes(items[6])) || (greenClicked.includes(items[1]) && greenClicked.includes(items[4]) && greenClicked.includes(items[7])) || ((greenClicked.includes(items[2]) && greenClicked.includes(items[5]) && greenClicked.includes(items[8])) || (greenClicked.includes(items[2]) && greenClicked.includes(items[4]) && greenClicked.includes(items[6])) || (greenClicked.includes(items[0]) && greenClicked.includes(items[4]) && greenClicked.includes(items[8]))
     )) {
 
-        alert('congragulations yo green')
+        // alert('congragulations yo green')
+        console.log('leave green gamestatus')
+        winner = 0;
         return false;
 
 
@@ -82,17 +97,26 @@ const gameStatus = function () {
     if ((redClicked.includes(items[0]) && redClicked.includes(items[1]) && redClicked.includes(items[2])) || (redClicked.includes(items[3]) && redClicked.includes(items[4]) && redClicked.includes(items[5])) || (redClicked.includes(items[6]) && redClicked.includes(items[7]) && redClicked.includes(items[8])) || (redClicked.includes(items[0]) && redClicked.includes(items[3]) && redClicked.includes(items[6])) || (redClicked.includes(items[1]) && redClicked.includes(items[4]) && redClicked.includes(items[7])) || ((redClicked.includes(items[2]) && redClicked.includes(items[5]) && redClicked.includes(items[8])) || (redClicked.includes(items[2]) && redClicked.includes(items[4]) && redClicked.includes(items[6])) || (redClicked.includes(items[0]) && redClicked.includes(items[4]) && redClicked.includes(items[8]))
     )) {
 
-        alert('congragulations yo red')
+        // alert('congragulations yo red')
+        console.log('leave red gamestatus')
+
+        winner = 1;
         return false;
     }
     if (clickedItems.length == 9) {
-        alert('no more there, tie the tie')
+        // alert('no more there, tie the tie')
+        console.log('leave tie gamestatus')
+
         return false;
     }
+    console.log('leave gamestatus')
+
     return true;
 }
 
 const dumbComp = function () {
+    console.log('enter dumbComp')
+
     if (clickedItems.length < 9) {
         let selection = items[Math.floor(Math.random() * 9)];
         let i = 0;
@@ -112,10 +136,27 @@ const dumbComp = function () {
             }
         }
     }
-    setTimeout(gamePlay, 10);
-}
 
+    // setTimeout(gamePlay, 10);
+    console.log('leave dumbComp')
+
+}
+const announceIt = function () {
+    if (winner == 2) {
+        console.log('tie side')
+        alert('This is a tie')
+    }
+    else {
+        console.log('winner side')
+        alert('Congraulation yo ' + players[winner])
+    }
+    const x = prompt('wanna play again? y for yes, or enter anything to exit');
+    restIt(x);
+    return console.log('got Delayed');
+}
 const changeTurn = function () {
+    console.log('enter changeTurn')
+
     if (clickedItems.length < 9) {
         if (playerGreen) {
             playerGreen = !playerGreen;
@@ -126,19 +167,25 @@ const changeTurn = function () {
             playerRed = !playerRed;
         }
     }
+    console.log('leave changeTurn')
+
 }
 
 const gamePlay = function () {
+    console.log('enter gamePlay')
+
     if (gameStatus()) {
         changeTurn()
-        if (playerRed) {
-            dumbComp()
-        }
+        dumbComp()
+        changeTurn()
     }
-    else {
-        const x = prompt('wanna play again? y for yes, or enter anything to exit');
-        restIt(x);
+    if (!gameStatus()) {
+        setTimeout(announceIt, 10);
+
+
     }
+    console.log('leave gamePlay')
+
 }
 
 const playIt = function () {
